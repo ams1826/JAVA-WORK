dc45c3381ceb5c749b9715810607504c34912753
ams1826 <akashsangwan1826@gmail.com>
2022-04-03 11:35:18 +0530

    update

--- primefactors.java
+++ primefactors.java
@@ -5,16 +5,14 @@ public class primefactors {
         int a = scn.nextInt();
 //if we use i*i then only print half prime factors 512 = 1 2 4 8 16.......
 //whereas other factors are 32 64 128 256 512 which are not printing
-        for(int i=1; i<=a; i++){
-            if(a%i==0){
+        for(int i=2; i*i<=a; i++){
+            while(a%i==0){
                 System.out.print(i+" ");
+                a = a/i; 
             }
         }
-        if(a==1){
-            System.out.println("it is not prime");
-        }
-        else{
-            System.out.println("it is prime");
+        if(a!=1){
+            System.out.println(a);
         }
     }
 }
