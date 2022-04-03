42bd169dafdcde4d604e33e3e1e5e5fe8acc69ab
ams1826 <akashsangwan1826@gmail.com>
2022-04-03 12:52:15 +0530

    update

--- pattern4.java
+++ pattern4.java
@@ -1,30 +1,30 @@
-//Printing X
+//Print star diagonally form left to right
 
 import java.util.Scanner;
-public class pattern6 {
+public class pattern4 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         for(int r=1; r<=n; r++){
             for(int c=1; c<=n; c++){
-                if(r==c || (r+c)==(n+1)){
+                if(r==c){
                     System.out.print("*\t");
                 }else{
                     System.out.print("\t");
                 }
             }
             System.out.println();
-            System.out.println();
         }
     }
 }
 
+
 /*
 
-*   *
- * *
+*
+ *
   *
- * *
-*   *
+   *
+    *
 
 */
