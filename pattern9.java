5829b0e17065ffc7a4cb48c1ed07e20bcd4fad6f
ams1826 <akashsangwan1826@gmail.com>
2022-04-03 14:16:54 +0530

    update

---
+++ pattern9.java
@@ -0,0 +1,41 @@
+//Printing kite pattern
+
+import java.util.Scanner;
+public class pattern9 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int r= 1 , spaces = n/2 , stars = 1;
+        while(r<=n){
+        //spaces
+            for(int i=1; i<=spaces; i++){
+                System.out.print("\t");
+            }
+        //stars
+            for(int j=1; j<=stars; j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+            if(r<=n/2){
+                spaces--;
+                stars+=2;
+            }
+            else{
+                spaces++;
+                stars-=2;
+            }
+            r++;
+        }
+    }
+}
+
+
+/*
+
+  *
+ ***
+*****
+ ***
+  *
+  
+*/
