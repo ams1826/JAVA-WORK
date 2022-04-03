1d7fa9fd296cf29f7ee2bd41189d67cb66990949
ams1826 <akashsangwan1826@gmail.com>
2022-04-03 11:57:54 +0530

    update

--- pattern2.java
+++ pattern2.java
@@ -1,18 +1,12 @@
-/*
-*
-**
-***
-****
-*****
-*/
-
 import java.util.Scanner;
-public class pattern1 {
+public class pattern2 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=i; j++){
+        int stars=0;
+        for(int i=1; i<=n; i++){
+            stars=(n+1)-i;
+            for(int j=1; j<=stars; j++){
                 System.out.print("*\t");
             }
             System.out.println();
