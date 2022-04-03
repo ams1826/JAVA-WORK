3bdf96c29b02e8c070b361e58578d1b6f0cb1469
ams1826 <akashsangwan1826@gmail.com>
2022-04-03 11:37:50 +0530

    update

--- primefactorisation.java
+++ primefactorisation.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-public class primefactors {
+public class primefactorisation {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
-//if we use i*i then only print half prime factors 512 = 1 2 4 8 16.......
-//whereas other factors are 32 64 128 256 512 which are not printing
         for(int i=2; i*i<=a; i++){
             while(a%i==0){
                 System.out.print(i+" ");
