9c74ed62a784ea925732d66f41e28a7d2957274c
ams1826 <akashsangwan1826@gmail.com>
2022-04-07 23:44:25 +0530

    update

--- pattern16.java
+++ pattern16.java
@@ -1,3 +1,33 @@
+//Printing swastik
+
+import java.util.Scanner;
 public class pattern16 {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int row=1 ;
+        while(row<=n){
+            for(int i=1; i<=n; i++){
+                if(row==n/2+1 || i==n/2+1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+            row++;
+        }
+    }
 }
+
+
+/*
+
+* * *   *
+    *   *
+* * * * *
+*   *
+*   * * *
+
+*/
