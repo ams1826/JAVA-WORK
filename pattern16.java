505f22b56f5a1082e3220e1e5ef9afb9206e2943
ams1826 <akashsangwan1826@gmail.com>
2022-04-08 14:45:12 +0530

    update

--- pattern16.java
+++ pattern16.java
@@ -8,26 +8,52 @@ public class pattern16 {
         int row=1 ;
         while(row<=n){
             for(int i=1; i<=n; i++){
-                if(i!=n/2 || row==(n/2)+1){
-                    if((i>(n/2)+1 && row==n/2) || (row>(n/2)+1 && i==n-1))
-                    {
-                        System.out.print("  ");
-                    }else{
+//Printing 1st half i.e. above n/2+1
+//  *   * * *
+//  *   *
+                if(row<=n/2){
+                    if((i==1 || i>=n/2+1) && row==1){
                         System.out.print("* ");
                     }
-                    
-                }else{
-                    System.out.print("  ");
+                    else{
+                        if(i==1 || i==n/2+1){
+                            System.out.print("* ");
+                        }
+                        else{
+                            System.out.print("  ");
+                        }
+                    }
+                }
+//Printing n/2+1 row
+//  *   * * *
+//  *   *
+//  * * * * *
+                else if(row==n/2+1){
+                        System.out.print("* ");
+                }
+//Printing 2nd half i.e. below n/2+1 row
+//  *   * * *
+//  *   *
+//  * * * * *
+//      *   *
+//  * * *   *
+                else{
+                    if(row==n && (i<=n/2+1 || i==n)){
+                        System.out.print("* ");
+                    }
+                    else if(i==n/2+1 || i==n){
+                        System.out.print("* ");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
                 }
             }
             System.out.println();
-
             row++;
         }
     }
 }
-
-
 /*
 
 *   * * *
