732a0098ba7a1af7a2a70da3f2e3028effc47711
ams1826 <akashsangwan1826@gmail.com>
2022-04-20 00:26:00 +0530

    update

--- pattern3.java
+++ pattern3.java
@@ -5,11 +5,9 @@ public class pattern3 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int stars = 0;
-        int spaces = 0;
         for(int i=1; i<=n; i++){
-            stars=i;
-            spaces = n-i;
+            int stars=i;
+            int spaces = n-i;
             for(int j=1; j<=spaces; j++){
                 System.out.print("\t");
             }
