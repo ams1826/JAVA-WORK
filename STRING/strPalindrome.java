f48926d9afcb4db3ed518c746ecf155dd91cfc30
ams1826 <akashsangwan1826@gmail.com>
2022-04-23 13:09:02 +0530

    update

--- STRING/strPalindrome.java
+++ STRING/strPalindrome.java
@@ -1,15 +1,31 @@
 package STRING;
 
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class strPalindrome {
+
+    public static boolean isPallindrome(String ss){
+        int left = 0;
+        int right = ss.length()-1;
+        while(left<right){
+            if(ss.charAt(left)==ss.charAt(right)){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String s = scn.next();
         for(int i=0; i<s.length(); i++){
             for(int j=i+1; j<=s.length(); j++){
                 String ss = s.substring(i,j);
-                System.out.println(ss+" ");
+                if(isPallindrome(ss)==true){
+                    System.out.println(ss+" ");
+                }
             }
             System.out.println();
         }
