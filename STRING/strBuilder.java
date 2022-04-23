ee53b01e57b71585eaecaae1f032cd236441efa8
ams1826 <akashsangwan1826@gmail.com>
2022-04-23 19:28:28 +0530

    update

---
+++ STRING/strBuilder.java
@@ -0,0 +1,12 @@
+package STRING;
+
+import java.util.*;
+public class strBuilder {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        StringBuilder sd = new StringBuilder();
+        sd.append("abcd");
+        System.out.println(sd);
+        
+    }
+}
---
+++ STRING/strrepetitionINT.java
@@ -0,0 +1,38 @@
+package STRING;
+
+import java.util.*;
+public class strrepetitionINT {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        int p=0;
+        int count=1;
+        String ss = ""+str.charAt(0);
+        while(p+1<str.length()){
+            if(str.charAt(p)==str.charAt(p+1)){
+                count++;
+            }else{
+                if(count>1){
+                    ss = ss + count + str.charAt(p+1);
+                    count = 1;
+                }else{
+                    ss = ss + str.charAt(p+1);
+                    count = 1;
+                }
+            }
+            p++;
+        }
+        if(count>1){
+            ss = ss + count;
+        }
+        System.out.println(ss);
+    }
+}
+
+/*
+
+wwwwaaadexxxxxx
+
+w4a3dex6
+
+*/
