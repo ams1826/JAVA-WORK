0e7eb5c3b58218391e837fd707c53f54cac799bc
ams1826 <akashsangwan1826@gmail.com>
2022-04-23 12:56:53 +0530

    update

---
+++ STRING/strPalindrome.java
@@ -0,0 +1,17 @@
+package STRING;
+
+import java.util.Scanner;
+
+public class strPalindrome {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.next();
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1; j<=s.length(); j++){
+                String ss = s.substring(i,j);
+                System.out.println(ss+" ");
+            }
+            System.out.println();
+        }
+    }
+}
