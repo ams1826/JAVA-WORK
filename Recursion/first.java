187f19982a94479ce34e59353ea44532cdb0267b
ams1826 <akashsangwan1826@gmail.com>
2022-05-08 12:56:32 +0530

    update

---
+++ Recursion/first.java
@@ -0,0 +1,16 @@
+package Recursion;
+import java.util.*;
+public class first {
+    public static void print(int n) {
+        if(n==0){
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        print(n);
+    }
+}
