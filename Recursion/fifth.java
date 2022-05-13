504e9b62ef07f6e09c66efa1f0a9e74f5286ed40
ams1826 <akashsangwan1826@gmail.com>
2022-05-14 02:51:07 +0530

    update

---
+++ Recursion/fifth.java
@@ -0,0 +1,31 @@
+//Printing number raise to some power using recursion
+// 2^5 = 32
+
+package Recursion;
+import java.util.*;
+public class fifth {
+    public static int power(int n , int p){
+        if(p==0){
+            return 1;
+        }
+        int po = power(n , p-1);
+        int m = n * po;
+        // System.out.println(n +" "+ p+" "+po+" "+m);
+        return m;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int p = scn.nextInt();
+        int ans = power(n,p/2);
+    //EVEN
+        if(p%2==0){
+            ans*=ans;
+        }
+    //ODD
+        else{
+            ans*=ans*n;
+        }
+        System.out.print(ans);
+    }
+}
