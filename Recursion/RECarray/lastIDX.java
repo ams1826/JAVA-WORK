b7d91bb4ea899eff5499edf6760b45fe43fcb970
ams1826 <akashsangwan1826@gmail.com>
2022-05-19 23:07:58 +0530

    update

--- Recursion/RECarray/lastIDX.java
+++ Recursion/RECarray/lastIDX.java
@@ -1,5 +1,13 @@
 package Recursion.RECarray;
 
 public class lastIDX {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+//array input
+        for(int i=0;i<n;i++){
+           arr[i]=scn.nextInt();
+        }
+    }
 }
