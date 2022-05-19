d1c48c7ad4e3cf627e677694e71b96f1e8ae6dee
ams1826 <akashsangwan1826@gmail.com>
2022-05-20 00:16:26 +0530

    update

--- Basic/inverseposition.java
+++ Basic/inverseposition.java
@@ -1,4 +1,14 @@
-//Position * (10^[digit-1])   ---->  position * (int)Math.pow(10,digit-1)
+/*changing position of the numbers according to index
+FORMULA-->[Position * (10^[digit-1])   ---->  position * (int)Math.pow(10,digit-1)]
+            I/P:-   5 2 1 3 4
+  their indexes:-   5 4 3 2 1   INDEX(to number)     NUMBER(to index)
+            O/P:-   5 1 2 4 3 { number 1 is placed at index 4
+                                number 2 is placed at index 3
+                                number 3 is placed at index 1
+                                number 4 is placed at index 2
+                                number 5 is placed at index 5
+                                    }
+*/
 
 import java.util.Scanner;
 public class inverseposition {
