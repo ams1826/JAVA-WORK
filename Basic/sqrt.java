1eda78583d08301d14ca823630d5c74cb158b1f6
ams1826 <akashsangwan1826@gmail.com>
2022-05-21 08:33:50 +0530

    update

--- Basic/sqrt.java
+++ Basic/sqrt.java
@@ -1,24 +1,10 @@
 import java.util.Scanner;
 public class sqrt {
     public static void main(String[] args) {
-/*
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int i = 2;
-        int count = 1;
-        while(i*i<n){
-            i++;
-            if(i*i>n){
-                System.out.println(i-1);
-                count = 0;
-            }
-        }
-        if(count==1){
-            System.out.print(i);
-        }
-  */
   int i=0;
-  while(i*i<=2147395600)
+  while(i*i<n)
  {
      i++;
  }
