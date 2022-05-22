014841fa64bcf30dfa6aaa03cc0bb47620ddf0d8
ams1826 <akashsangwan1826@gmail.com>
2022-05-22 12:10:08 +0530

    update

--- STRING/anagram.java
+++ STRING/anagram.java
@@ -6,12 +6,30 @@ public class anagram {
         Scanner scn = new Scanner(System.in);
         String str1 = scn.next();
         String str2 = scn.next();
-        System.out.print(anagm(str1, str2));
+        int l1 = str1.length();
+        int l2 = str2.length();
+        int arr1[] = new int[26];
+        int arr2[] = new int[26];
+        for(int i=0 ; i<l1 ; i++){
+            int idx = str1.charAt(i) - 'a';
+            arr1[idx]++;
+        }
+        for(int i=0 ; i<l2 ; i++){
+            int idx = str2.charAt(i) - 'a';
+            arr2[idx]++;
+        }
+        for(int i=0 ; i<26 ; i++){
+            if(arr1[i] != arr2[i]){
+                System.out.print("not anagram");
+                return ;
+            }
+        }
+        System.out.print("number is anagram");
     }
 }
 
 
 
 
-in phone pic
-21 / 05 / 2021
+// in phone pic
+// 21 / 05 / 2021
