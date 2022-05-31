0b7ef7d184629b25fc9796330e6b905058486148
ams1826 <akashsangwan1826@gmail.com>
2022-06-01 00:47:01 +0530

    update

--- Recursion/RECarray/allIDX.java
+++ Recursion/RECarray/allIDX.java
@@ -1,28 +1,6 @@
 import java.util.*;
 public class allIDX {
     public static void main(String[] args) {
-        if(arr.length < 0){
-            return -1;
-        }
-        if(arr[idx] == a){
-            return idx;
-        }
-        return last(arr, idx-1 , a);
-    }
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int arr[] = new int[n];
-//array input
-        for(int i=0;i<n;i++){
-           arr[i]=scn.nextInt();
-        }
- //nummber to search
-        System.out.print("Enter value to search");
-        int a = scn.nextInt();
-//calling the function
-        int ans = last(arr , arr.length-1 , a);
-        System.out.println(ans);
+        
     }
 }
