f4239f42512b1f7f5e120107942178cc96c13163
ams1826 <akashsangwan1826@gmail.com>
2022-06-04 20:49:14 +0530

    update

--- Recursion/RECarray/allIDX.java
+++ Recursion/RECarray/allIDX.java
@@ -1,6 +1,44 @@
 import java.util.*;
 public class allIDX {
     public static void main(String[] args) {
-        
+        //Finding first index of a value occuring multiple times in array
+// I/P : 1 2 3 3 4 3
+// search : 3
+// O/P : index 2
+
+package Recursion.RECarray;
+import java.util.*;
+public class firstIDX {
+
+//Finding maximum element
+    public static int first(int arr[] , int idx , int a) {
+        if(arr.length == idx){
+            return -1;
+        }
+        if(arr[idx] == a){
+            return idx;
+        }
+        return first(arr, idx+1 , a);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+//array input
+        for(int i=0;i<n;i++){
+           arr[i]=scn.nextInt();
+        }
+//nummber to search
+        System.out.print("Enter value to search");
+        int a = scn.nextInt();
+//calling the function
+        int ans = first(arr , 0 , a);
+        System.out.println(ans);
+    }
+}
+
+
     }
 }
