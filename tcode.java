4c163d19c721e11d7a2b6b153be16bdc20958cc7
ams1826 <akashsangwan1826@gmail.com>
2022-06-16 23:17:32 +0530

    update

--- tcode.java
+++ tcode.java
@@ -3,39 +3,13 @@
 import java.util.Scanner;
 public class tcode {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int row=1,stars=1,spaces=n-1;
-        while(row<=n){
-            //spaces
-            for(int i = 1;i<=spaces;i++){
-                System.out.print(" ");
-            }
-
-            //stars
-            for(int i = 1;i<=stars;i++){
-                if(i%2==0){
-                    System.out.print(" ");
-                }else{
-                    System.out.print("*");
-                }
-                
-            }
-            System.out.println();
-            //preparing for the next row
-            stars+=2;
-            spaces--;
-            row++;
-            
-        }
-        
+    System.out.println("\n");
+    System.out.println("\t"+"Java Programming Language");
+    System.out.println("\t"+"Python Programming Language");
+    System.out.println("\t"+"C Programming Language");
+    System.out.println("\n");
     }
 }
 
 
-/*
-   *
-  * *
- * * *
-* * * *
-*/
+
