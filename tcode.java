4a044e4598325bd8f01da82579a5ab6085acc2d9
ams1826 <akashsangwan1826@gmail.com>
2022-06-16 23:18:09 +0530

    update

--- tcode.java
+++ tcode.java
@@ -2,11 +2,5 @@
 
 import java.util.Scanner;
 public class tcode {
-    public static void main(String[] args) {
-    System.out.println("\n");
-    System.out.println("\t"+"Java Programming Language");
-    System.out.println("\t"+"Python Programming Language");
-    System.out.println("\t"+"C Programming Language");
-    System.out.println("\n");
-    }
+
 }
