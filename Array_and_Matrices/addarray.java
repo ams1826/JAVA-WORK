3f5f43229d70bc106344ac4c3d7a2f35309dfdc6
ams1826 <akashsangwan1826@gmail.com>
2022-06-23 22:25:14 +0530

    update

--- Array_and_Matrices/addarray.java
+++ Array_and_Matrices/addarray.java
@@ -50,3 +50,16 @@ public class addarray {
         return res;
     }
 }
+
+
+
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n1 = scn.nextInt();
+        int arr1[] = new int[n1];
+        for(int i=0;i<n1;i++){
+           arr1[i]=scn.nextInt();
+        }
+
+}
