23f53ab6f8ccc6b6c591a080b6a731ebaf01a94a
ams1826 <akashsangwan1826@gmail.com>
2022-06-27 23:37:52 +0530

    update

---
+++ Array_and_Matrices/cFILE.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+    int L = 18;
+    int B = 26;
+    int ans = L * B;
+    cout<<"Area of rectangle is "<<ans;
+    return 0;
+}
