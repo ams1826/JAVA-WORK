514d5f4214e4fe6913b8b7a1e10485196b300aca
ams1826 <akashsangwan1826@gmail.com>
2022-09-02 02:36:55 +0530

    p

---
+++ Array_and_Matrices/matADDpt.java
@@ -0,0 +1,45 @@
+public class matADDpt {
+
+    public static void add(int mat[][] , int n){
+        int r = 1;
+        int count = 0;
+        for(int  c=1; c<=n; c++){
+                //adding top wall_______________________________________
+            if(r==1){
+                count = count + mat[r-1][c-1];
+                if(c==n){
+                    r++;
+                    c=0;
+                }
+            }
+                //adding bottom wall____________________________________
+            else if(r==n)
+            count = count + mat[r-1][c-1];
+
+                //adding side wall______________________________________
+            else if((c==1) || (c==n)){
+                count = count + mat[r-1][c-1];
+                if(c==n){
+                    r++;
+                    c=0;
+                }
+            }
+
+            else if((r==c) || (r+c)==(n+1))
+            count = count + mat[r-1][c-1];
+        }
+
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt;
+        int mat[][] = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                mat[i][j] = scn.nextInt();
+            }
+        }
+    }
+}
