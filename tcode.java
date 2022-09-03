bfd89432443c0222c2b92cf3c0dd46f7ea9bab6e
ams1826 <akashsangwan1826@gmail.com>
2022-09-03 19:50:17 +0530

    p

--- tcode.java
+++ tcode.java
@@ -33,3 +33,37 @@ public class tcode {
         }
     }
 }
+
+
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+public class tcode {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int i=3;
+        int row=1;
+        System.out.println("1");
+        for(int j=2; j<=i; j++){
+
+            if(j==(n+n-1)){
+                System.out.print(j);
+                break;
+            }
+
+            else if(j==i){
+                System.out.print(j+"\n");
+                i = i + 2;
+                row++;
+                j = i - row - 1;
+            }
+
+            else{
+                System.out.print(j+" ");
+            }
+
+        }
+    }
+}
