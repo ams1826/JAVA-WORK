36ed71391ef927f9ace597e658c38aec16b3277f
ams1826 <akashsangwan1826@gmail.com>
2022-09-03 19:51:52 +0530

    p

--- tcode.java
+++ tcode.java
@@ -16,17 +16,20 @@ public class tcode {
         int i=1;
         for(int j=1; j<=i; j++){
 
+                //base condition________________________
             if(j==(n+n-1)){
                 System.out.print(j);
                 break;
             }
 
+                //when ele in row ends__________________
             else if(j==i){
                 System.out.print(j+"\n");
                 i = i + 2;
                 j = (i/2);
             }
 
+                //print ele of row______________________
             else
             System.out.print(j+" ");
 
