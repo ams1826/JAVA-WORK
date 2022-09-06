34fa3a19c224a7e6949642614d0735a84e8eb652
ams1826 <akashsangwan1826@gmail.com>
2022-09-07 02:17:04 +0530

    p

--- PATTERNS/pattern25.java
+++ PATTERNS/pattern25.java
@@ -1,5 +1,63 @@
 package PATTERNS;
 
 public class pattern25 {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int i=1;
+        for(int j=1; j<=i; j++){
+
+                //base condition________________________
+            if(j==(n+n-1)){
+                System.out.print(j);
+                break;
+            }
+
+                //when ele in row ends__________________
+            else if(j==i){
+                System.out.print(j+"\n");
+                i = i + 2;
+                j = (i/2);
+            }
+
+                //print ele of row______________________
+            else
+            System.out.print(j+" ");
+
+        }
+    }
 }
+
+
+
+// import java.util.Scanner;
+
+// import javax.lang.model.util.ElementScanner14;
+// public class tcode {
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         int i=3;
+//         int row=1;
+//         System.out.println("1");
+//         for(int j=2; j<=i; j++){
+
+//             if(j==(n+n-1)){
+//                 System.out.print(j);
+//                 break;
+//             }
+
+//             else if(j==i){
+//                 System.out.print(j+"\n");
+//                 i = i + 2;
+//                 row++;
+//                 j = i - row - 1;
+//             }
+
+//             else{
+//                 System.out.print(j+" ");
+//             }
+
+//         }
+//     }
+// }
