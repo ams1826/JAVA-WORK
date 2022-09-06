d7bb26466262b938765ec178a6eba7671150fe3d
ams1826 <akashsangwan1826@gmail.com>
2022-09-07 02:25:50 +0530

    p

--- tcode.java
+++ tcode.java
@@ -8,27 +8,11 @@ import java.util.Scanner;
 public class tcode {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int i=1;
-        for(int j=1; j<=i; j++){
-
-                //base condition________________________
-            if(j==(n+n-1)){
-                System.out.print(j);
-                break;
-            }
-
-                //when ele in row ends__________________
-            else if(j==i){
-                System.out.print(j+"\n");
-                i = i + 2;
-                j = (i/2);
-            }
-
-                //print ele of row______________________
-            else
-            System.out.print(j+" ");
-
+        int N = scn.nextInt();
+        int n = N;
+        int bit = 0;
+        while(){
+            bit = bit + 
         }
     }
 }
