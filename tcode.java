5062097b5c5e83544e81c93d830aa3cdeb6decf0
ams1826 <akashsangwan1826@gmail.com>
2022-09-07 23:44:14 +0530

    p

--- tcode.java
+++ tcode.java
@@ -4,15 +4,20 @@ binary print in 8 bit form
 
 */
 
-import java.util.Scanner;
+import java.util.*;
 public class tcode {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int N = scn.nextInt();
         int n = N;
         int bit = 0;
-        while(){
-            bit = bit + 
+        int i = 0;
+        while(n!=0){
+            int r = n%2;
+            bit = bit + ((int)Math.pow(10,i)*r);
+            i++;
+            n = n/2;
         }
+        System.out.println(bit);
     }
 }
