dc83d2c433ca89ae8e230052d618a14a2b39de29
ams1826 <akashsangwan1826@gmail.com>
2023-06-24 20:27:07 +0530

    p

--- Binary_Trees/tree6iterate.java
+++ Binary_Trees/tree6iterate.java
@@ -1,8 +1,8 @@
 package Binary_Trees;
 
-import java.util.*;
-public class tree1 {
+import java.util.Stack;
 
+public class tree6iterate {
     public static class Node {
         int data;
         Node left;
@@ -15,8 +15,6 @@ public class tree1 {
         }
     }
 
-
-
     public static class Pair {
         Node node;
         int state;
@@ -26,6 +24,36 @@ public class tree1 {
             this.state = state;
         }
     }
+
+
+
+
+//----------------------------------------ITERATE----------------------------------------//
+
+    public static void iteratePrePostIn(Node node) {
+        Stack<Pair> st = new Stack<>();
+        Pair rtp = new Pair(node, 1);
+        st.push();
+
+        while(st.size() > 0){
+            Pair top = st.peek();
+            if(top.state == 1){         //Pre, s++, left
+
+            }
+            else if(top.state == 2){    //In, s++, right
+
+            }
+            else{                       //Post, pop
+
+            }
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+//----------------------------------------ITERATE----------------------------------------//
+
 
 
 
@@ -40,7 +68,6 @@ public class tree1 {
         int idx = 0;
         while(st.size() > 0){
             Pair top = st.peek();
-
 
             if(top.state == 1){
                 idx++;
@@ -80,5 +107,6 @@ public class tree1 {
                 st.pop();
             }
         }
+        display(root);
     }
 }
