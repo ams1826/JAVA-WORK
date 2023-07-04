e27b5e844eb30cd7602b80547360a72a40a3085f
ams1826 <akashsangwan1826@gmail.com>
2023-07-05 00:08:15 +0530

    p

---
+++ trypy.py
@@ -0,0 +1,41 @@
+# values = input("enter values")
+# list = values.split(",")
+# tupl = tuple(list)
+# print(list)
+# print(tupl)
+# print(list[0])
+# print(list[len(list)-1])
+
+
+# filename = input("Input the Filename: ")
+# f_extns = filename.split(".")
+# print ("The extension of the file is : " + f_extns[-1])
+
+
+# li = ['11','12','2014']
+# a = "/".join(li)
+# print(a)
+
+
+# import re
+# text = 'how are you?'
+# matched = re.match(r'(.+)',text)
+# print(matched.group())
+
+
+# import calendar
+# y = int(input())
+# m = int(input())
+# print(calendar.month(y,m))
+
+
+# from datetime import date
+# f_date = date(2014, 3, 30)
+# l_date = date(2014, 4, 21)
+# delta = l_date - f_date
+# print(delta.days)
+
+
+# print(abs(-20))
+
+#   asignment phase 1 string   1   11  13  1
