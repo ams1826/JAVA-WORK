ffe7f8bc317f227ebc62f48e2e807e09cc9960be
ams1826 <akashsangwan1826@gmail.com>
2023-08-24 14:49:42 +0530

    p

--- STRING/noRepeatSubString.java
+++ STRING/noRepeatSubString.java
@@ -3,20 +3,27 @@
 import java.util.Scanner;
 
 public class noRepeatSubString {
+
+    public static String subStringNoRepeat(String str) {
+        StringBuilder result = new StringBuilder();
+        boolean[] seenChar = new boolean[26];
+        for(char c : str.toCharArray()){
+            if(!seenChar[c - 'a']){
+                seenChar[c - 'a'] = true;
+                result.append(c);
+                // System.out.println(c);
+            }
+        }
+        return result.toString();
+    }
+
+
+
+
     public static void main(String[] args) {
         try (Scanner scn = new Scanner(System.in)) {
             String str = scn.next();
-            int p1=1;
-            String ss = ""+str.charAt(0);
-            while(p1<str.length()){
-                if(str.charAt(p1)==str.charAt(p1-1)){
-                    p1++;
-                }else{
-                    ss = ss + str.charAt(p1);
-                    p1++;
-                }
-            }
-            System.out.println(ss);
+            System.out.println(subStringNoRepeat(str));
         }
         catch(Exception e){
             System.out.println("Error occured " + e.getMessage());
